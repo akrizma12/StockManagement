3b056be111da264e3206086adbbbb3e7e3e38618
Karishma Pokhrel <karishma.pokhrel@bestbuy.com>
2021-05-18 17:54:08 -0500

    Application starts

---
+++ src/main/java/com/akrizma/stocktracker/StockTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.akrizma.stocktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrackerApplication.class, args);
+	}
+}
